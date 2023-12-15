f63cb97d3df7de6b6177ff6fb34afded079c125c
BORA <70184893+rara-record@users.noreply.github.com>
2023-12-15 18:06:27 +0900

    feat: home layout

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from 'react'
+import style from './home.module.css'
+import TabProvider from '@/app/(afterLogin)/home/context/TabProvider'
+import Tab from '@/app/(afterLogin)/home/_component/Tab'
+import PostForm from '@/app/(afterLogin)/home/_component/PostForm'
+import Post from '@/app/(afterLogin)/_component/Post'
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
-  );
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  )
 }
 
-export default Home;
+export default Home
