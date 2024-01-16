470dce1d3014a668d1056e3052c4f8020c4fd7d5
BORA <70184893+rara-record@users.noreply.github.com>
2024-01-16 15:25:36 +0900

    feat: server component로 사용하는 방법

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,39 +1,10 @@
-'use client'
-
 import style from './signup.module.css'
-import { redirect, useRouter } from 'next/navigation'
-import { ChangeEventHandler, FormEventHandler, useState } from 'react'
+import { redirect } from 'next/navigation'
+import BackButton from '@/app/(beforeLogin)/_component/BackButton'
 
 export default function SignupModal() {
-  const [id, setId] = useState('')
-  const [password, setPassword] = useState('')
-  const [nickname, setNickname] = useState('')
-  const [image, setImage] = useState('')
-  const [imageFile, setImageFile] = useState<File>()
-
-  const router = useRouter()
-  const onClickClose = () => {
-    router.back()
-    // TODO: 뒤로가기가 /home이 아니면 /home으로 보내기
-  }
-
-  const onChangeId: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setId(e.target.value)
-  }
-
-  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setPassword(e.target.value)
-  }
-  const onChangeNickname: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setNickname(e.target.value)
-  }
-  const onChangeImageFile: ChangeEventHandler<HTMLInputElement> = (e) => {
-    e.target.files && setImageFile(e.target.files[0])
-  }
-
-  const onSubmit = async (formData: FormData) => {
+  const submit = async (formData: FormData) => {
     'use server'
-
     let shouldRedirect = false
 
     try {
@@ -58,80 +29,40 @@ export default function SignupModal() {
       <div className={style.modalBackground}>
         <div className={style.modal}>
           <div className={style.modalHeader}>
-            <button className={style.closeButton} onClick={onClickClose}>
-              <svg
-                width={24}
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-                className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"
-              >
-                <g>
-                  <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
-                </g>
-              </svg>
-            </button>
+            <BackButton />
             <div>계정을 생성하세요.</div>
           </div>
-          <form action={onSubmit}>
+          <form action={submit}>
             <div className={style.modalBody}>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="id">
                   아이디
                 </label>
-                <input
-                  id="id"
-                  className={style.input}
-                  type="text"
-                  placeholder=""
-                  value={id}
-                  onChange={onChangeId}
-                  required
-                />
+                <input id="id" name="id" className={style.input} type="text" placeholder="" required />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="name">
                   닉네임
                 </label>
-                <input
-                  id="name"
-                  className={style.input}
-                  type="text"
-                  placeholder=""
-                  value={nickname}
-                  onChange={onChangeNickname}
-                  required
-                />
+                <input id="name" name="name" className={style.input} type="text" placeholder="" required />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="password">
                   비밀번호
                 </label>
-                <input
-                  id="password"
-                  className={style.input}
-                  type="password"
-                  placeholder=""
-                  value={password}
-                  onChange={onChangePassword}
-                  required
-                />
+                <input id="password" name="password" className={style.input} type="password" placeholder="" required />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="image">
                   프로필
                 </label>
-                <input
-                  id="image"
-                  className={style.input}
-                  type="file"
-                  accept="image/*"
-                  onChange={onChangeImageFile}
-                  required
-                />
+                <input id="image" name="image" className={style.input} type="file" accept="image/*" required />
               </div>
             </div>
             <div className={style.modalFooter}>
-              <button className={style.actionButton}>가입하기</button>
+              <button type="submit" className={style.actionButton}>
+                가입하기
+              </button>
             </div>
           </form>
         </div>
