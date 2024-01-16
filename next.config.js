738651df27fb397e026f1bc69a2c0e3f17f872be
BORA <70184893+rara-record@users.noreply.github.com>
2024-01-16 15:06:37 +0900

    chore: next config serverActions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {{experimental: {serverActions: boolean}}} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,  // server action을 사용하려면 활성화
+    // serverActionsBodySizeLimit: '2mb', // 최대 요청 본문은 기본적으로 1MB이며 필요한 경우 제한할 수 있음
+
+  },
+}
+
 
 module.exports = nextConfig
