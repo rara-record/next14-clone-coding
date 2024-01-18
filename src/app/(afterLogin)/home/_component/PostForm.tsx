813c9c0c2cb97eb3b5e9c34d2c52fd780dea2d17
BORA <a52447879@gmail.com>
2024-01-18 23:20:30 +0900

    fix: 이미지 태그에 w,h

--- src/app/(afterLogin)/home/_component/PostForm.tsx
+++ src/app/(afterLogin)/home/_component/PostForm.tsx
@@ -1,59 +1,59 @@
-'use client'
+'use client';
 
-import { ChangeEventHandler, FormEventHandler, useRef, useState } from 'react'
-import Image from 'next/image'
+import { ChangeEventHandler, FormEventHandler, useRef, useState } from 'react';
+import Image from 'next/image';
 
-import style from './styles/postForm.module.css'
+import style from './styles/postForm.module.css';
 
 const PostForm = () => {
-  const imageRef = useRef<HTMLInputElement>(null)
-  const [content, setContent] = useState('')
+  const imageRef = useRef<HTMLInputElement>(null);
+  const [content, setContent] = useState('');
   const me = {
     id: 'zerohch0',
     image: '/img/5Udwvqim.jpg',
-  }
+  };
 
   const onChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    setContent(e.target.value)
-  }
+    setContent(e.target.value);
+  };
 
   const onSubmit: FormEventHandler = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
   const onClickButton = () => {
-    imageRef.current?.click()
-  }
+    imageRef.current?.click();
+  };
 
   return (
     <form className={style.postForm} onSubmit={onSubmit}>
       <div className={style.postUserSection}>
         <div className={style.postUserImage}>
-          <Image src={me.image} alt={me.id} />
+          <Image src={me.image} alt={me.id} width={100} height={100} />
         </div>
       </div>
       <div className={style.postInputSection}>
-        <textarea value={content} onChange={onChange} placeholder="무슨 일이 일어나고 있나요?" />
+        <textarea value={content} onChange={onChange} placeholder='무슨 일이 일어나고 있나요?' />
         <div className={style.postButtonSection}>
           <div className={style.footerButtons}>
             <div className={style.footerButtonLeft}>
-              <input type="file" name="imageFiles" multiple hidden ref={imageRef} />
-              <button className={style.uploadButton} type="button" onClick={onClickButton}>
-                <svg width={24} viewBox="0 0 24 24" aria-hidden="true">
+              <input type='file' name='imageFiles' multiple hidden ref={imageRef} />
+              <button className={style.uploadButton} type='button' onClick={onClickButton}>
+                <svg width={24} viewBox='0 0 24 24' aria-hidden='true'>
                   <g>
-                    <path d="M3 5.5C3 4.119 4.119 3 5.5 3h13C19.881 3 21 4.119 21 5.5v13c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 21 3 19.881 3 18.5v-13zM5.5 5c-.276 0-.5.224-.5.5v9.086l3-3 3 3 5-5 3 3V5.5c0-.276-.224-.5-.5-.5h-13zM19 15.414l-3-3-5 5-3-3-3 3V18.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-3.086zM9.75 7C8.784 7 8 7.784 8 8.75s.784 1.75 1.75 1.75 1.75-.784 1.75-1.75S10.716 7 9.75 7z" />
+                    <path d='M3 5.5C3 4.119 4.119 3 5.5 3h13C19.881 3 21 4.119 21 5.5v13c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 21 3 19.881 3 18.5v-13zM5.5 5c-.276 0-.5.224-.5.5v9.086l3-3 3 3 5-5 3 3V5.5c0-.276-.224-.5-.5-.5h-13zM19 15.414l-3-3-5 5-3-3-3 3V18.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-3.086zM9.75 7C8.784 7 8 7.784 8 8.75s.784 1.75 1.75 1.75 1.75-.784 1.75-1.75S10.716 7 9.75 7z' />
                   </g>
                 </svg>
               </button>
             </div>
-            <button type="submit" className={style.actionButton} disabled={!content}>
+            <button type='submit' className={style.actionButton} disabled={!content}>
               게시하기
             </button>
           </div>
         </div>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default  PostForm
+export default PostForm;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,15 @@
-import { auth } from './auth'
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
+
+import { auth } from './auth';
 
 export async function middleware() {
-  const session = await auth()
+  const session = await auth();
   if (!session) {
     // 로그인 안했으면 돌아가거라
-    return NextResponse.redirect('http://localhost:3000/i/flow/login')
+    return NextResponse.redirect('http://localhost:3000/i/flow/login');
   }
 }
 
 export const config = {
   matcher: ['/compose/tweet', '/home', '/explore', '/messages', '/search'],
-}
+};
