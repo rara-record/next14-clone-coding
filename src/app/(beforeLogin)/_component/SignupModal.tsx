8f5df7686807b34951ba07570985c97692710c39
BORA <70184893+rara-record@users.noreply.github.com>
2024-01-18 18:18:22 +0900

    fix: type error

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,34 +1,35 @@
-'use client'
+'use client';
 
-import { useFormState, useFormStatus } from 'react-dom'
+import React from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
 
-import style from './signup.module.css'
-import BackButton from '@/app/(beforeLogin)/_component/BackButton'
-import onSubmit from '@/app/(beforeLogin)/_lib/signup'
+import style from './signup.module.css';
+import BackButton from '@/app/(beforeLogin)/_component/BackButton';
+import onSubmit from '@/app/(beforeLogin)/_lib/signup';
 
-const showMessage = (message: string) => {
+const showMessage = (message: string | null) => {
   if (message === 'no_id') {
-    return '아이디를 입력하세요.'
+    return '아이디를 입력하세요.';
   }
   if (message === 'no_name') {
-    return '닉네임을 입력하세요.'
+    return '닉네임을 입력하세요.';
   }
   if (message === 'no_password') {
-    return '비밀번호를 입력하세요.'
+    return '비밀번호를 입력하세요.';
   }
   if (message === 'no_image') {
-    return '이미지를 업로드하세요.'
+    return '이미지를 업로드하세요.';
   }
   if (message === 'user_exists') {
-    return '이미 사용 중인 아이디입니다.'
+    return '이미 사용 중인 아이디입니다.';
   }
 
-  return { message: null }
-}
+  return null;
+};
 
 const SignupModal = () => {
-  const [state, formAction] = useFormState(onSubmit, { message: null })
-  const { pending } = useFormStatus()
+  const [state, formAction] = useFormState(onSubmit, { message: null });
+  const { pending } = useFormStatus();
 
   return (
     <div className={style.modalBackground}>
@@ -40,32 +41,32 @@ const SignupModal = () => {
         <form action={formAction}>
           <div className={style.modalBody}>
             <div className={style.inputDiv}>
-              <label className={style.inputLabel} htmlFor="id">
+              <label className={style.inputLabel} htmlFor='id'>
                 아이디
               </label>
-              <input id="id" name="id" className={style.input} type="text" placeholder="" required />
+              <input id='id' name='id' className={style.input} type='text' placeholder='' required />
             </div>
             <div className={style.inputDiv}>
-              <label className={style.inputLabel} htmlFor="name">
+              <label className={style.inputLabel} htmlFor='name'>
                 닉네임
               </label>
-              <input id="name" name="name" className={style.input} type="text" placeholder="" />
+              <input id='name' name='name' className={style.input} type='text' placeholder='' />
             </div>
             <div className={style.inputDiv}>
-              <label className={style.inputLabel} htmlFor="password">
+              <label className={style.inputLabel} htmlFor='password'>
                 비밀번호
               </label>
-              <input id="password" name="password" className={style.input} type="password" placeholder="" />
+              <input id='password' name='password' className={style.input} type='password' placeholder='' />
             </div>
             <div className={style.inputDiv}>
-              <label className={style.inputLabel} htmlFor="image">
+              <label className={style.inputLabel} htmlFor='image'>
                 프로필
               </label>
-              <input id="image" name="image" className={style.input} type="file" accept="image/*" />
+              <input id='image' name='image' className={style.input} type='file' accept='image/*' />
             </div>
           </div>
           <div className={style.modalFooter}>
-            <button type="submit" className={style.actionButton} disabled={pending}>
+            <button type='submit' className={style.actionButton} disabled={pending}>
               가입하기
             </button>
             <div className={style.error}>{showMessage(state.message || '')}</div>
@@ -73,7 +74,7 @@ const SignupModal = () => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignupModal
+export default SignupModal;
--- src/app/_component/MSWComponent.ts
+++ src/app/_component/MSWComponent.ts
@@ -1,14 +1,16 @@
-'use client'
-import { useEffect } from 'react'
+'use client';
+
+import { useEffect } from 'react';
 
 export const MSWComponent = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-        require('@/mocks/browser')
+        // eslint-disable-next-line global-require
+        require('@/mocks/browser');
       }
     }
-  }, [])
+  }, []);
 
-  return null
-}
+  return null;
+};
