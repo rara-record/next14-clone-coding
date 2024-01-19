62e71249b0d0e401890dbd8c431ded6406fd8ba0
BORA <70184893+rara-record@users.noreply.github.com>
2024-01-19 14:15:33 +0900

    feat: AuthSessionProvider 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 
 import './globals.css';
 import { MSWComponent } from '@/app/_component/MSWComponent';
+import AuthSession from '@/app/_component/AuthSession';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,7 +22,7 @@ const RootLayout = ({ children }: Props) => {
     <html lang='en'>
       <body className={inter.className}>
         <MSWComponent />
-        {children}
+        <AuthSession>{children}</AuthSession>
       </body>
     </html>
   );
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,9 @@
-import { http, HttpResponse } from 'msw'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { http, HttpResponse } from 'msw';
 
 export const handlers = [
   http.post('/api/login', () => {
-    console.log('로그인')
+    console.log('로그인');
     return HttpResponse.json(
       {
         id: 'bora',
@@ -13,19 +14,19 @@ export const handlers = [
         headers: {
           'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/', // HTTP header 로 쿠키 설정하는 방법. 값을 넣어줄 때
         },
-      }
-    )
+      },
+    );
   }),
   http.post('/api/logout', () => {
-    console.log('로그아웃')
+    console.log('로그아웃');
     return new HttpResponse(null, {
       headers: {
         'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0', // 값을 넣지 않을 때
       },
-    })
+    });
   }),
   http.post('/api/users', async ({ request }) => {
-    console.log('회원가입')
+    console.log('회원가입');
     // return HttpResponse.text(JSON.stringify('user_exists'), {
     //   status: 403,
     // })
@@ -33,6 +34,6 @@ export const handlers = [
       headers: {
         'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/;Max-Age=0',
       },
-    })
+    });
   }),
-]
+];
