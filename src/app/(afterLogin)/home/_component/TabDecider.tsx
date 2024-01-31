bea9c0a9e61086e1a161c8719058d95c2ebf3999
BORA <a52447879@gmail.com>
2024-01-31 22:33:18 +0900

    feat: 다이나믹 쿼리

--- src/app/(afterLogin)/home/_component/TabDecider.tsx
+++ src/app/(afterLogin)/home/_component/TabDecider.tsx
@@ -6,11 +6,13 @@ import { TabContext } from '@/app/(afterLogin)/home/_component/TabProvider';
 import PostRecommends from '@/app/(afterLogin)/home/_component/PostRecommends';
 import FollowingPosts from '@/app/(afterLogin)/home/_component/FollowingPosts';
 
-export default function TabDecider() {
+const TabDecider = () => {
   const { tab } = useContext(TabContext);
 
   if (tab === 'rec') {
     return <PostRecommends />;
   }
   return <FollowingPosts />;
-}
+};
+
+export default TabDecider;
--- src/app/(afterLogin)/search/_lib/getSearchResult.ts
+++ src/app/(afterLogin)/search/_lib/getSearchResult.ts
@@ -2,9 +2,14 @@ import { QueryFunction } from '@tanstack/query-core';
 
 import { Post } from '@/model/Post';
 
+/**
+ * 리액트 쿼리에는 객체가 들어갈 수 있지만, next 태그에서는 객체가 들어갈 수 없어서
+ * searchParams.q 로 넣어주어야 한다.
+ * */
+
 export const getSearchResult: QueryFunction<
   Post[],
-  [_1: string, _2: string, searchParams: { q: string; pf?: string; f?: string }]
+  [_1: string, _2: string, searchParams: { q: string; f?: string; pf?: string }]
 > = async ({ queryKey }) => {
   const [_1, _2, searchParams] = queryKey;
   const res = await fetch(`http://localhost:9090/api/search/${searchParams.q}?${searchParams.toString()}`, {
